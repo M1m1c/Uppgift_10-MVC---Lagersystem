bb5c9687a9ff85ee5ecdfd0b6fe0312e93c2e8cf
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-04-28 17:14:43 +0200

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required,MaxLength(60)]
+        public string Name { get; set; }
+        [Required, Range(0,1000)]
+        public int Price { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public string Shelf { get; set; }
+
+        [Required, Range(0, 1000)]
+        public int Count { get; set; }
+
+        public string Description { get; set; }
+    }
+}
